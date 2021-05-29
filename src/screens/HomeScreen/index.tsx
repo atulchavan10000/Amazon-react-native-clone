fb0a797b996a67bc1a87ba8a81e601e9f2e361cb
Atul <atulchavan528@gmail.com>
2021-05-29 21:09:50 +0530

    🚸 FlatList for product list

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, } from 'react-native'
+import { View, StyleSheet, FlatList} from 'react-native'
 import ProductItem from '../../components/ProductItem';
 import products from '../../data/products';
 
 const HomeScreen = () => {
     return (
         <View style={styles.page}>
-           <ProductItem item={products[0]}/>
-           
+           <FlatList
+                data = {products}
+                renderItem={({item}) => <ProductItem item={item}/>}
+           />
         </View>
     );
 };
