b37a67c635c018ea3d31e4773b8439f16306e59d
Atul <atulchavan528@gmail.com>
2021-05-29 21:04:03 +0530

    ♻️ making ProductItem receive dynamic data

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -2,27 +2,45 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
-const ProductItem = () => {
-    return (
+
+interface ProductItemProps{
+    item: {
+        id: string;
+        title: string;
+        image: string;
+        avgRating: number;
+        ratings: number;
+        price: number;
+        oldPrice?: number
+    }
+}
+const ProductItem = ({item} : ProductItemProps) => {
+       return (
         <View style={styles.root}>
-        <Image style={styles.image} source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleanarchitecture.jpg' }}/>
-        <View style={styles.rightContainer}>
-            <Text style={styles.title} numberOfLines={3}>Logitech MX Master 3 Advanced Wireless Mouse for Mac - Bluetooth/USB</Text>
-            {/* Ratings */}
-            <View style={styles.ratingsContainer}>
-                <FontAwesome style={styles.star} name="star" size={18} color={"#e47911"}/>
-                <FontAwesome style={styles.star} name="star" size={18} color={"#e47911"}/>
-                <FontAwesome style={styles.star} name="star" size={18} color={"#e47911"}/>
-                <FontAwesome style={styles.star} name="star-half-full" size={18} color={"#e47911"}/>
-                <FontAwesome style={styles.star} name="star-o" size={18} color={"#e47911"}/>
-                <Text>13,032</Text>
+            <Image style={styles.image} source={{uri : item.image}}/>
+            <View style={styles.rightContainer}>
+                <Text style={styles.title} numberOfLines={3}>{item.title}</Text>
+                {/* Ratings */}
+                <View style={styles.ratingsContainer}>
+                    { [0,0,0,0,0].map((el, index) => 
+                        <FontAwesome 
+                            style={styles.star} 
+                            name={index < Math.floor(item.avgRating) ? 'star' : 'star-o'} 
+                            /*ToDo: add logic for half star */
+                            size={18} 
+                            color={"#e47911"}
+                        />) 
+                    }
+                    <Text>{item.ratings}</Text>
+                </View>
+                <Text style={styles.price}>
+                    from ${item.price}
+                    {item.oldPrice && (
+                        <Text style={styles.oldPrice}> ${item.oldPrice}</Text>
+                    )}
+                </Text>
             </View>
-            <Text style={styles.price}>
-                from $13.59
-                <Text style={styles.oldPrice}> $16.22</Text>
-            </Text>
         </View>
-    </View>
     )
 }
 
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, } from 'react-native'
 import ProductItem from '../../components/ProductItem';
+import products from '../../data/products';
 
 const HomeScreen = () => {
     return (
         <View style={styles.page}>
-           <ProductItem/>
-           <ProductItem/>
-           <ProductItem/>
-           <ProductItem/>
-           <ProductItem/>
+           <ProductItem item={products[0]}/>
+           
         </View>
     );
 };
