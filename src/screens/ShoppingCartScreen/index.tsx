0653c5d6bca016a6a8bc7cd9e4aa2e52860d80a4
Atul <atulchavan528@gmail.com>
2021-05-30 12:21:15 +0530

    🚧  proceed to checkout button

--- src/screens/ShoppingCartScreen/index.tsx
+++ src/screens/ShoppingCartScreen/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, StyleSheet, FlatList} from 'react-native';
+import { View, StyleSheet, FlatList, Text} from 'react-native';
 import CartProductItem from '../../components/CartProductItem';
 import products from '../../data/cart';
-const HomeScreen = () => {
+import Button from '../../components/Button';
+
+const ShoppingCartScreen = () => {
+    const totalPrice = products.reduce(
+        (summedPrice, product) => (
+            summedPrice + product.item.price * product.quantity
+        ), 
+    0);
     return (
         <View style={styles.page}>
+            <View>
+                <Text style={{fontSize: 18}} >
+                    Subtotal ({products.length} items): {' '}
+                    <Text style={{color: '#e47911', fontWeight:'bold'}}>${totalPrice.toFixed(2)}</Text>
+                </Text>
+                <Button 
+                    text="Proceed to checkout" 
+                    onPress={() => console.warn('go to checkout')}
+                    containerStyles={{backgroundColor: '#f7e300', borderColor: '#c7b702'}}
+                />
+
+            </View>
            <FlatList
                 data = {products}
                 renderItem={({item}) => <CartProductItem cartItem={item}/>}
@@ -21,4 +40,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default HomeScreen;
+export default ShoppingCartScreen;
